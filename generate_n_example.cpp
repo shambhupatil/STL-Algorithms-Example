6c5063a24090b9413efe8cb838556f7bdae68386
Shambhulinganagouda Patil <shambhuspatil@gmail.com>
2024-05-04 17:54:36 -0700

    generate_n

--- generate_n_example.cpp
+++ generate_n_example.cpp
@@ -23,13 +23,14 @@ void case2() {
 }
 
 void case3() {
-    std::vector<std::string> v; // If not initialized use back inserter
+    std::vector<std::string> v; // If not initialized use back inserter but not recommended
     std::generate_n(std::back_inserter(v), 10, [a = std::string(""), b = '`']()mutable {++b; a += b; return a; });
     print(v);
 }
 
 void case4() {
     std::vector<int> v(10); // Use back_inserter if not initialized of N greater than v.size()
+    //DONT USE back_inserter
     std::generate_n(std::back_inserter(v), 20, []() {return 11; });
     print(v);
 }
